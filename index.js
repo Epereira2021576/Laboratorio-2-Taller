8a4bc675ddfabdffce46f93526d361a9bf6fcb0b
Epereira2021576 <epereira-2021576@kinal.edu.gt>
2024-02-12 09:15:51 -0600

    Finished everything related to server running

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Probando si levanta');
+const Server = require('./models/server');
+
+require('dotenv').config();
+
+const server = new Server();
+
+// Calling the listen() method from the Server class to start the server
+server.listen();
