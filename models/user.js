0c78c15ebe7b958570410aaa843eefaa2e4bac10
Epereira2021576 <epereira-2021576@kinal.edu.gt>
2024-02-18 19:21:58 -0600

    Modified the user model

--- models/user.js
+++ models/user.js
@@ -27,12 +27,34 @@ const UserSchema = Schema({
 
   course: [
     {
-      type: String,
-      required: [true, 'Course is required'],
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
     },
   ],
 });
 
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = bcryptjs.genSaltSync();
+    this.password = bcryptjs.hashSync(this.password, salt);
+  }
+  next();
+});
+
 const User = mongoose.model('User', UserSchema);
 
-module.exports = User;
+const courseSchema = Schema({
+  courseName: {
+    type: String,
+    required: [true, 'Course name is required'],
+  },
+
+  courseTeacher: {
+    type: String,
+    required: [true, 'Course teacher is required'],
+  },
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+(module.exports = User), Course;
