a412e3b4347cb31c501d8e3cbe3fcc2a966273ce
Epereira2021576 <epereira-2021576@kinal.edu.gt>
2024-02-18 21:48:05 -0600

    Added functions to db-validators

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const { User, Course } = require('../models/user');
 
 const emailExists = async (email = '') => {
   const emailUser = await User.findOne({ email });
@@ -12,6 +12,20 @@ const userExistsById = async (id = '') => {
 
   if (!user) {
     throw new Error(`The user with the id ${id} does not exist`);
+  }
+};
+
+const courseNameExists = async (name = '') => {
+  const course = await Course.findOne({ name });
+  if (course) {
+    throw new Error(`The course name ${name} already exists`);
+  }
+};
+
+const courseExistsById = async (id = '') => {
+  const course = await Course.findById(id);
+  if (!course) {
+    throw new Error(`The course with id ${id} does not exist`);
   }
 };
 
